89acf72f7dcd350c9ee0902cb2867b76576c3496
Semih Senan <50872469+Semcester@users.noreply.github.com>
2022-04-24 19:40:12 +0300

    Delete Proje directory

--- Proje/personal-business-tracking/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import jobsReducer from "../features/JobSlice"
-
-export const store = configureStore({
-  reducer: {
-    jobs: jobsReducer,
-  },
-})
